c121584263d839fb0ff60c32fef077566e9160d8
yue19870813 <yue19870813@163.com>
2016-05-13 17:08:10 +0800

    修改配置，真机允许测试。

--- Innovate/Classes/HelloWorldScene.cpp
+++ Innovate/Classes/HelloWorldScene.cpp
@@ -38,8 +38,8 @@ bool HelloWorld::init()
 
     // add a "close" icon to exit the progress. it's an autorelease object
     auto closeItem = MenuItemImage::create(
-                                           "CloseNormal.png",
-                                           "CloseSelected.png",
+                                           "res/CloseNormal.png",
+                                           "res/CloseSelected.png",
                                            CC_CALLBACK_1(HelloWorld::menuCloseCallback, this));
     
 	closeItem->setPosition(Vec2(origin.x + visibleSize.width - closeItem->getContentSize().width/2 ,
@@ -66,7 +66,7 @@ bool HelloWorld::init()
     this->addChild(label, 1);
 
     // add "HelloWorld" splash screen"
-    auto sprite = Sprite::create("HelloWorld.png");
+    auto sprite = Sprite::create("res/HelloWorld.png");
 
     // position the sprite on the center of the screen
     sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
