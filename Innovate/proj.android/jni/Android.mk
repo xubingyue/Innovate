603b42c4582c77ef3bae3d132c84bc617e70097b
yue19870813 <yue19870813@163.com>
2016-06-12 15:20:21 +0800

    android配置文件

--- Innovate/proj.android/jni/Android.mk
+++ Innovate/proj.android/jni/Android.mk
@@ -12,7 +12,56 @@ LOCAL_MODULE_FILENAME := libcocos2dcpp
 
 LOCAL_SRC_FILES := hellocpp/main.cpp \
                    ../../Classes/AppDelegate.cpp \
-                   ../../Classes/HelloWorldScene.cpp
+                   ../../Classes/LoadingScene.cpp \
+                   ../../Classes/LayerManager.cpp \
+                   ../../Classes/LocalDataManager.cpp \
+                   ../../Classes/HelloWorldScene.cpp \
+                   ../../Classes/core/algorithm/AStarFindPath.cpp \
+                   ../../Classes/core/utils/md5.cpp \
+                   ../../Classes/core/utils/LocalDataUtil.cpp \
+                   ../../Classes/core/utils/StringUtil.cpp \
+                   ../../Classes/core/utils/LanguageUtil.cpp \
+                   ../../Classes/core/memory/MemManager.cpp \
+                   ../../Classes/core/data/database/libcsv.c \
+                   ../../Classes/core/data/database/TableHelper.cpp \
+                   ../../Classes/core/data/DataManager.cpp \
+                   ../../Classes/core/data/Config.cpp \
+                   ../../Classes/core/data/Element.cpp \
+                   ../../Classes/core/data/Drop_item.cpp \
+                   ../../Classes/core/data/Drop.cpp \
+                   ../../Classes/core/data/Raid_map.cpp \
+                   ../../Classes/core/data/Scene_map.cpp \
+                   ../../Classes/core/data/Elf.cpp \
+                   ../../Classes/core/data/Language.cpp \
+                   ../../Classes/core/data/Monster.cpp \
+                   ../../Classes/core/data/Object.cpp \
+                   ../../Classes/core/data/Role.cpp \
+                   ../../Classes/core/data/Stage.cpp \
+                   ../../Classes/model/MonsterModel.cpp \
+                   ../../Classes/model/ModelBase.cpp \
+                   ../../Classes/model/PlayerModel.cpp \
+                   ../../Classes/model/GlobalModel.cpp \
+                   ../../Classes/model/BagModel.cpp \
+                   ../../Classes/role/RoleSprite.cpp \
+                   ../../Classes/map/StageMapView.cpp \
+                   ../../Classes/map/MapObjBase.cpp \
+                   ../../Classes/map/MapObjDisplay.cpp \
+                   ../../Classes/map/MapObjBuilding.cpp \
+                   ../../Classes/map/MapObjMonster.cpp \
+                   ../../Classes/battle/BattleObj/BattleObjBase.cpp \
+                   ../../Classes/battle/BattleObj/BattleMonster.cpp \
+                   ../../Classes/battle/BattleObj/BattlePlayer.cpp \
+                   ../../Classes/battle/BattleObj/BattleElf.cpp \
+                   ../../Classes/battle/BattleController.cpp \
+                   ../../Classes/battle/BattleView.cpp \
+                   ../../Classes/ui/UIComponent.cpp \
+                   ../../Classes/ui/PlayerView.cpp \
+                   ../../Classes/ui/BigCrystalView.cpp \
+                   ../../Classes/ui/BagView.cpp \
+                   ../../Classes/ui/BattleResultView.cpp \
+                   ../../Classes/ui/MapTransferView.cpp \
+                   ../../Classes/utils/IconUtil.cpp
+
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
 
